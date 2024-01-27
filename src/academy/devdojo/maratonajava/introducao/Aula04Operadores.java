85118f940bf0e4e3ab4ea9e0bf0b4aab728059d3
Cida Luna <cidalunna@hotmail.com>
2024-01-27 13:40:09 -0300

    Aula 17 Maratona Java - Operadores lógicos

--- src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
+++ src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
@@ -2,7 +2,7 @@ package academy.devdojo.maratonajava.introducao;
 
 public class Aula04Operadores {
     public static void main(String[] args) {
-        // + - * /
+        // Operadores aritméticos  + - * /
         int numero01 = 10;
         int numero02 = 20;
         double soma = numero01 + numero02;
@@ -14,17 +14,37 @@ public class Aula04Operadores {
         int resto = 21 % 7;
         System.out.println("Resto: " +resto);
 
-        // <> <= >= == !=
+        // Operadores relacionais do Java  <> <= >= == !=
         boolean isDezMaiorDoQueVinte = 10 > 20;
         boolean isDezMenorDoQueVinte = 10 < 20;
+        boolean isDezMaiorIgualVinte = 10 >= 20;
 
         boolean isDezIgualVinte = 10 == 20;
         boolean isDezIgualDez = 10 == 10;
 
         System.out.println("isDezMaiorDoQueVinte: "+isDezMaiorDoQueVinte);
         System.out.println("isDezMenorDoQueVinte: "+isDezMenorDoQueVinte);
+        System.out.println("isDezMaiorIgualVinte: "+isDezMaiorIgualVinte);
         System.out.println("isDezIgualVinte: "+isDezIgualVinte);
         System.out.println("isDezIgualDez: "+isDezIgualDez);
+
+        int idade = 29;
+        float salario = 3380F;
+        boolean isDentroDaLeiMaiorQueTrinta = idade > 30 && salario >= 4612;
+        boolean isDentroDaLeiMenorQueTrinta = idade < 30 && salario >= 3381;
+        System.out.println("isDentroDaLeiMaiorQueTrinta: " +isDentroDaLeiMaiorQueTrinta);
+        System.out.println("isDentroDaLeiMenorQueTrinta: " +isDentroDaLeiMenorQueTrinta);
+
+        // Operadores Lógicos:
+        // Temos o (AND) && , (OR) || e (NO) !
+        double valorTotalContaCorrente = 200;
+        double valorTotalContaPoupanca = 10000;
+        float valorPlaystation = 5000F;
+        boolean isPlaystationCincoCompravel = valorTotalContaCorrente > valorPlaystation || valorTotalContaPoupanca > valorPlaystation;
+
+        System.out.println("isPlaystationCincoCompravel: "+isPlaystationCincoCompravel);
+
+
 
     }
 }
