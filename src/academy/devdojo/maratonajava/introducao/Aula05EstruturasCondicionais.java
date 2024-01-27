5f102075670add125c6f4eb5ae4fa77c9d27ec20
Cida Luna <cidalunna@hotmail.com>
2024-01-27 15:16:06 -0300

    Aula 21 Maratona Java - Estruturas Condicionais IF, ELSE IF

--- src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais.java
+++ src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais.java
@@ -2,9 +2,28 @@ package academy.devdojo.maratonajava.introducao;
 
 public class Aula05EstruturasCondicionais {
     public static void main(String[] args) {
-        if(true){
-            System.out.println("Dentro do IF");
+        int idade = 15;
+        String categoria;
+        boolean isAutorizadoTerHabilitacao = idade >= 18;
+        if(isAutorizadoTerHabilitacao){
+            System.out.println("Idade "+idade+" anos, está autorizado(a) a tirar CNH");
         }
+
+        if(!isAutorizadoTerHabilitacao){
+            System.out.println("Não autorizado(a) a tirar CNH!");
+        }
+
+        if(idade <15){
+            categoria = "Categoria Infantil";
+            System.out.println(categoria);
+        }else if(idade >= 15 && idade < 18){
+            categoria = "Catergoria Juvenil";
+            System.out.println(categoria);
+        }else{
+            categoria = "Categoria Adulto";
+            System.out.println(categoria);
+        }
+
         System.out.println("Fora do IF");
     }
 }
