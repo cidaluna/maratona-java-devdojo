53b70f7f82314b76cc032538bf5025d90bde5959
Cida Luna <cidalunna@hotmail.com>
2024-01-28 19:01:18 -0300

    Aula 38 Maratona Java - Inicializando arrays multidimensionais add valores

--- src/academy/devdojo/maratonajava/introducao/Aula08ArraysMultidimensionais02.java
+++ src/academy/devdojo/maratonajava/introducao/Aula08ArraysMultidimensionais02.java
@@ -5,7 +5,7 @@ public class Aula08ArraysMultidimensionais02 {
         int[][] arrayInt = new int[3][]; // o array j é de tamanho indefinido
 
         arrayInt[0] = new int[2];  // esse new int[2] significa que é um array j que tem 2 posições
-        arrayInt[1] = new int[3];  // array j tem 3 posições
+        arrayInt[1] = new int[]{1,2,3};  // array j tem 3 posições
         arrayInt[2] = new int[6];  // array j tem 6 posições
 
         for(int[] arrBase: arrayInt){
