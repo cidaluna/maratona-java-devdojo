bd87ab77239f6e2e7c84c696a17f0504e2538d92
Cida Luna <cidalunna@hotmail.com>
2024-01-28 17:26:46 -0300

    Aula 32 Maratona Java - Arrays início

---
+++ src/academy/devdojo/maratonajava/introducao/Aula07Arrays01.java
@@ -0,0 +1,9 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula07Arrays01 {
+    public static void main(String[] args) {
+        // somente tipos de referência podem ser inicializados com null
+        int[] idades = null;
+        System.out.println(idades);
+    }
+}
