2040bce8f6d57e2f7478bb006014cad64c5dbb91
Cida Luna <cidalunna@hotmail.com>
2024-01-28 09:40:03 -0300

    Aula 25 Maratona Java - Estruturas Condicionais SWITCH

---
+++ src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais05.java
@@ -0,0 +1,47 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula05EstruturasCondicionais05 {
+    public static void main(String[] args) {
+        // imprima os dias da semana considerando 1 como domingo
+        byte dia = 5;
+        // char, int, byte, short, enum, String
+        switch (dia){
+            case 1:
+                System.out.println("Domingo");
+                break;
+            case 2:
+                System.out.println("Segunda");
+                break;
+            case 3:
+                System.out.println("Terça");
+                break;
+            case 4:
+                System.out.println("Quarta");
+                break;
+            case 5:
+                System.out.println("Quinta");
+                break;
+            case 6:
+                System.out.println("Sexta");
+                break;
+            case 7:
+                System.out.println("Sábado");
+                break;
+            default:
+                System.out.println("Opção inválida!");
+        }
+
+
+        char sexo = 'M';
+        switch(sexo){
+            case 'M':
+                System.out.println("Masculino");
+                break;
+            default: // a posição do default não interfere no resultado
+                System.out.println("Inválido");
+            case 'F':
+                System.out.println("Feminino");
+                break;
+        }
+    }
+}
