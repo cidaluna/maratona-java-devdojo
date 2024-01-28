b32ea43e7551662603e3859d2601d642e35802f0
Cida Luna <cidalunna@hotmail.com>
2024-01-28 19:50:04 -0300

    Aula 40  Maratona Java - Orientação a objetos parte 2

---
+++ src/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/dominio/Estudante.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.javacore.Aintroducaoclasses.dominio;
+
+public class Estudante {
+
+    public String nome = "Testes";
+    public int idade;
+    public char sexo;
+
+
+}
