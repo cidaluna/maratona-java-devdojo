a447fba54e7c4691e9b4e384693c4f59d3572457
Cida Luna <cidalunna@hotmail.com>
2024-01-28 11:26:12 -0300

    Aula 30 Maratona Java - Estruturas de Repetição utilizando break para sair do laço de repetição

---
+++ src/academy/devdojo/maratonajava/introducao/Aula06EstruturasDeRepeticao04.java
@@ -0,0 +1,31 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula06EstruturasDeRepeticao04 {
+    public static void main(String[] args) {
+        // Dado o valor de um carro, descubra em quantas vezes ele pode ser parcelado
+        // Condicao o valor da parcela tem que ser maior ou igual a mil
+        double valorCarro = 133459;
+        double parcelaMinima = 1000;
+        int qtdParcela;
+        double valorCadaParcela = 0;
+        System.out.println("---- Solução Cida Luna ----");
+        qtdParcela = (int)valorCarro / (int)parcelaMinima;
+        valorCadaParcela = valorCarro / qtdParcela;
+        System.out.println("Valor do carro: "+valorCarro);
+        System.out.println("Parcela mínima: "+parcelaMinima);
+        System.out.println("Total de parcela(s): "+qtdParcela);
+        System.out.println("Valor de Cada parcela: R$ "+valorCadaParcela);
+        System.out.println("--------------------------------");
+
+        System.out.println("---- Solução William Suane ----");
+        for(int parcela = 1; parcela <= valorCarro; parcela++){
+            double valorParcela = valorCarro / parcela;
+            if(valorParcela >= 1000){
+                System.out.println(parcela+ " parcela(s) de R$ "+valorParcela);
+            }
+            System.out.println("Fora do if, mas dentro do for "+parcela); // solucao nao ok, pq executa até chegar no valor total do carro
+        }
+
+
+    }
+}
