07e347e7cfa6b8202ecfd1d8c700e35f3809b348
Cida Luna <cidalunna@hotmail.com>
2024-01-29 09:05:34 -0300

    Aula 44 Maratona Java - Orientação a objetos introdução a métodos

---
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
@@ -0,0 +1,14 @@
+package academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio;
+
+public class Calculadora {
+
+    // modificador de acesso public, private
+    // retorno é diferente de saída, tem métodos que não tem saída embora tenha sido executado
+    public void somaDoisNumeros(){
+        System.out.println(10+10);
+    }
+
+    public void subtraiDoisMetodos(){
+        System.out.println(21-2);
+    }
+}
---
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/CalculadoraTest01.java
@@ -0,0 +1,15 @@
+package academy.devdojo.maratonajava.javacore.Bintroducaometodos.test;
+
+import academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio.Calculadora;
+
+public class CalculadoraTest01 {
+    public static void main(String[] args) {
+        // criando a variavel de referencia e o objeto
+        Calculadora calculadora = new Calculadora();
+        // utilizando a variavel de referencia e chamando o método soma
+        calculadora.somaDoisNumeros();
+        System.out.println("Finalizando CalculadoraTest01");
+
+        calculadora.subtraiDoisMetodos();
+    }
+}
