7c81ad8cee39a0190295861a97d5e5ed272dba23
Cida Luna <cidalunna@hotmail.com>
2024-01-29 09:33:17 -0300

    Aula 46 Maratona Java - Orientação a objetos metódos calculadora retorno

--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/CalculadoraTest02.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/CalculadoraTest02.java
@@ -2,13 +2,12 @@ package academy.devdojo.maratonajava.javacore.Bintroducaometodos.test;
 
 import academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio.Calculadora;
 
-public class CalculadoraTest01 {
+public class CalculadoraTest02 {
     public static void main(String[] args) {
         // criando a variavel de referencia e o objeto
         Calculadora calculadora = new Calculadora();
-        // utilizando a variavel de referencia e chamando o método soma
-        calculadora.somaDoisNumeros();
-        System.out.println("Finalizando CalculadoraTest01");
-        calculadora.subtraiDoisMetodos();
+        // utilizando a variavel de referencia e chamando o método multiplica passando 2 parâmetros
+        // ou seja,  num1 = 5 e num2 = 7
+        calculadora.multiplicaDoisNumeros(5,7);
     }
 }
---
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/CalculadoraTest03.java
@@ -0,0 +1,11 @@
+package academy.devdojo.maratonajava.javacore.Bintroducaometodos.test;
+
+import academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio.Calculadora;
+
+public class CalculadoraTest03 {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        double resultado = calculadora.divideDoisNumeros(10,2.5);
+        System.out.println(resultado);
+    }
+}
