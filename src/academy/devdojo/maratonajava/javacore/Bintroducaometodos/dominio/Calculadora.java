2a327980369989d1d557039b502e45b053e33db3
Cida Luna <cidalunna@hotmail.com>
2024-01-29 09:52:45 -0300

    Aula 47 Maratona Java - Orientação a objetos metódos calculadora retorno

--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
@@ -18,8 +18,40 @@ public class Calculadora {
 
     public double divideDoisNumeros(double num1, double num2){
         if(num2 == 0){
-            return 0;
+            return 0;  // William prefere dessa forma, pois para a execução logo no começo do método
         }
         return num1 / num2;
+    }
+
+    // formas diferentes de fazer o método de divisão
+    public double divideDoisNumeros02(double num1, double num2){
+        if(num2 == 0){
+            return 0;
+        }else{
+            return num1 / num2;
+        }
+    }
+
+    public double divideDoisNumeros03(double num1, double num2){
+        if(num2 != 0){
+            return num1 / num2;
+        }
+        return 0;
+    }
+
+    public void divideDoisNumeros04(double num1, double num2){ // método void sem retorno
+        if(num2 == 0){
+            System.out.println("Não existe divisão por zero!");
+        }else{
+            System.out.println(num1/num2);
+        }
+    }
+
+    public void divideDoisNumeros05(double num1, double num2){ // método void sem retorno
+        if(num2 == 0){
+            System.out.println("Não existe divisão por zero!");
+            return;   // funciona como se fosse o break; dentro do laço de repetição, ele para a execução
+        }
+        System.out.println(num1/num2);
     }
 }
--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/CalculadoraTest03.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/CalculadoraTest03.java
@@ -6,6 +6,19 @@ public class CalculadoraTest03 {
     public static void main(String[] args) {
         Calculadora calculadora = new Calculadora();
         double resultado = calculadora.divideDoisNumeros(10,2.5);
+        double resultado02 = calculadora.divideDoisNumeros02(10,2.5);
+        double resultado03 = calculadora.divideDoisNumeros03(10,2.5);
         System.out.println(resultado);
+        System.out.println(resultado02);
+        System.out.println(resultado03);
+        System.out.println("---------");
+        System.out.println("Método void exemplo 1: ");
+        calculadora.divideDoisNumeros04(10,2.5);
+        calculadora.divideDoisNumeros04(10,0);  // entrou no else
+        System.out.println("---------");
+        System.out.println("Método void exemplo 2: ");
+        // abaixo, utilizando o return; é como se fosse um break; dentro de um laço, ele para a execução
+        calculadora.divideDoisNumeros05(20,2.5);
+        calculadora.divideDoisNumeros05(20,0);  // entrou return;
     }
 }
