52dd8542f9eb6b1b87ea5aa82e0988c895fce165
Cida Luna <cidalunna@hotmail.com>
2024-01-31 11:40:05 -0300

    Aula 60 Maratona Java - Orientação a objetos, inicialização dos objetos

--- src/academy/devdojo/maratonajava/javacore/Dconstrutores/dominio/Anime.java
+++ src/academy/devdojo/maratonajava/javacore/Dconstrutores/dominio/Anime.java
@@ -5,17 +5,27 @@ public class Anime {
     private String nome;
     private String genero;
     private int episodios;
+    private String estudio;
 
     // no momento da criação do objeto, quero que seja obrigatório o preenchimento dos atributos, no construtor
     // caso não existisse essa regra, não precisaria criar o construtor, pq o Java cria um vazio por debaixo dos panos
    public Anime(String nome, String tipo, int episodios, String genero){  // construtor da classe
-        System.out.println("Dentro do construtor");
+        this();
         this.nome = nome;
         this.tipo = tipo;
         this.episodios = episodios;
         this.genero = genero;
+       System.out.println("Dentro do construtor com arg, this(vazio); e genero!!");
+    }
+    public Anime(String nome, String tipo, int episodios, String genero, String estudio){  // novo construtor da classe
+        this(nome, tipo, episodios, genero);
+        this.estudio = estudio;
+        System.out.println("Dentro do construtor com arg, this(preenchido) e estudio!!!");
     }
 
+    public Anime(){
+        System.out.println("Dentro do construtor sem argumentos!");
+    }
     public String getTipo() {
         return tipo;
     }
@@ -53,5 +63,6 @@ public class Anime {
         System.out.println(this.tipo);
         System.out.println(this.genero);
         System.out.println(this.episodios);
+        System.out.println(this.estudio);
     }
 }
--- src/academy/devdojo/maratonajava/javacore/Dconstrutores/test/AnimeTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Dconstrutores/test/AnimeTest01.java
@@ -5,7 +5,9 @@ import academy.devdojo.maratonajava.javacore.Dconstrutores.dominio.Anime;
 public class AnimeTest01 {
     public static void main(String[] args) {
 
-        Anime anime = new Anime("Haikyu", "TV", 12, "Ação");
+        Anime anime = new Anime("Haikyu", "TV", 12, "Ação", "IG");
+        System.out.println("-------------------------- ");
+        System.out.println("Imprimindo dados do anime: ");
         anime.imprime();
 
     }
