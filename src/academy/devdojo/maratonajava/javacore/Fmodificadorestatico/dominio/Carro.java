7d391c99a8a6dfc194c2969217e4ab011bf3a69f
Cida Luna <cidalunna@hotmail.com>
2024-01-31 12:35:30 -0300

    Aula 61 Maratona Java - Orientação a objetos, modificador static

---
+++ src/academy/devdojo/maratonajava/javacore/Fmodificadorestatico/dominio/Carro.java
@@ -0,0 +1,38 @@
+package academy.devdojo.maratonajava.javacore.Fmodificadorestatico.dominio;
+
+public class Carro {
+
+    // Fabricantes alemãs (exceto a porsch) o limite é de 250 km/h
+
+    private String nome;
+    private double velocidadeMaxima;
+    public static double velocidadeLimite = 250; // vai afetar todos os carros
+
+    public Carro(String nome, double velocidadeMaxima){
+        this.nome = nome;
+        this.velocidadeMaxima = velocidadeMaxima;
+    }
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getVelocidadeMaxima() {
+        return velocidadeMaxima;
+    }
+
+    public void imprime(){
+        System.out.println("--------------");
+        System.out.println("Nome: " + this.nome);
+        System.out.println("Velocidade Maxima: " + this.velocidadeMaxima);
+        System.out.println("Velocidade Limite: " + Carro.velocidadeLimite);
+    }
+
+    public void setVelocidadeMaxima(double velocidadeMaxima) {
+        this.velocidadeMaxima = velocidadeMaxima;
+    }
+
+}
---
+++ src/academy/devdojo/maratonajava/javacore/Fmodificadorestatico/test/CarroTest01.java
@@ -0,0 +1,22 @@
+package academy.devdojo.maratonajava.javacore.Fmodificadorestatico.test;
+
+import academy.devdojo.maratonajava.javacore.Fmodificadorestatico.dominio.Carro;
+
+public class CarroTest01 {
+    public static void main(String[] args) {
+
+        System.out.println("Teste de modificador de acesso estático: " + Carro.velocidadeLimite);
+        Carro c1 = new Carro("BMW", 280);
+        Carro c2 = new Carro("Mercedes", 275);
+        Carro c3 = new Carro("Audi", 290);
+
+        Carro.velocidadeLimite = 180; // vai afetar sobrescrever todos os objetos da classe Carro
+        // mesmo se nao existir nenhum objeto instanciado, Carro.velocidadeLimite existe e pode ser impresso se desejar
+        c1.imprime();
+        c2.imprime();
+        c3.imprime();
+
+        System.out.println("--------------");
+        System.out.println("Teste de modificador de acesso estático: " + Carro.velocidadeLimite);
+    }
+}
