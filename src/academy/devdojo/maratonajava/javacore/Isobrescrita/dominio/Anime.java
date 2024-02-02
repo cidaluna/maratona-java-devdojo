9d5276fbf80ed049a0b89839db6388455e450141
Cida Luna <cidalunna@hotmail.com>
2024-02-01 23:00:19 -0300

    Aula 77 Maratona Java - Orientação a objetos, sobrescrita de método, modificador final

---
+++ src/academy/devdojo/maratonajava/javacore/Isobrescrita/dominio/Anime.java
@@ -0,0 +1,23 @@
+package academy.devdojo.maratonajava.javacore.Isobrescrita.dominio;
+
+public class Anime {
+
+    private String nome;
+
+    @Override
+    public String toString(){
+          return "Anime "+ this.nome;
+    }
+
+    public Anime(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
---
+++ src/academy/devdojo/maratonajava/javacore/Jmodificadorfinal/dominio/Carro.java
@@ -0,0 +1,16 @@
+package academy.devdojo.maratonajava.javacore.Jmodificadorfinal.dominio;
+
+public class Carro {
+    private String nome;
+
+    // constante
+    public static final double VELOCIDADE_LIMITE = 250;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
