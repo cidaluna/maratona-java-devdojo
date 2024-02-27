c132fb7c6ac705038aabcceb2fcd201517293d2a
Cida Luna <cidalunna@hotmail.com>
2024-02-27 08:47:37 -0300

    Aula 144 Maratona Java - Classes utilitárias, Path

---
+++ src/academy/devdojo/maratonajava/javacore/Wnio/test/PathTest01.java
@@ -0,0 +1,18 @@
+package academy.devdojo.maratonajava.javacore.Wnio.test;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class PathTest01 {
+    public static void main(String[] args) {
+
+        Path p1 = Paths.get("file.txt");
+        Path p2 = Paths.get("C:\\Users\\DELL\\Documents\\spring-tools-projetos\\maratona-java-devdojo","file.txt");
+        Path p3 = Paths.get("C:", "\\Users\\DELL\\Documents\\spring-tools-projetos\\maratona-java-devdojo","file.txt");
+        Path p4 = Paths.get("C:","Users","DELL","Documents","spring-tools-projetos","maratona-java-devdojo","file.txt");
+        System.out.println(p1.getFileName());
+        System.out.println(p2.getFileName());
+        System.out.println(p3.getFileName());
+        System.out.println(p4.getFileName());
+    }
+}
